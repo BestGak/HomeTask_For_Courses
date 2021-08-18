abf6f34e75f44b6e77d3e1ad9314be47abe99579
BestGak <pro100gall@yandex.ru>
2021-08-18 14:29:46 +0300

    Add preloader for Optimization

--- Optimization/js/main.js
+++ Optimization/js/main.js
@@ -14,3 +14,11 @@ $('.single-item').slick({
   nextArrow: '<button type="button" class="slick-next"><img src="./img/arrow-left.png" alt=""></button>',
   prevArrow: '<button type="button" class="slick-prev"><img src="./img/arrow-right.png" alt=""></button>',
 });
+
+// window.onload = function () {
+//   document.body.classList.add('loaded_hiding');
+//   window.setTimeout(function () {
+//     document.body.classList.add('loaded');
+//     document.body.classList.remove('loaded_hiding');
+//   }, 500);
+// }
